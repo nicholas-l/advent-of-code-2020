d80980aae2690cad8595f852865988fabed5e937
Nicholas L <nicholas-l@users.noreply.github.com>
2020-12-02 20:45:46 +0000

    Ordering

--- src/day02.rs
+++ src/day02.rs
@@ -53,8 +53,8 @@ pub fn star_two(input: impl BufRead) -> usize {
 
 #[cfg(test)]
 mod tests {
-    use super::{star_one, star_two};
     use std::io::Cursor;
+    use super::{star_one, star_two};
 
     #[test]
     fn test_star_one() {
