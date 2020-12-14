b5482bb33eb6a7fa53558e3482e32226a1c9676f
Nicholas L <nicholas-l@users.noreply.github.com>
2020-12-14 18:58:53 +0000

    Tidy up day 14

--- src/day14.rs
+++ src/day14.rs
@@ -1,5 +1,6 @@
-use std::io::BufRead;
 use std::collections::HashMap;
+use std::convert::TryFrom;
+use std::io::BufRead;
 
 use regex::Regex;
 
@@ -7,48 +8,44 @@ lazy_static! {
     static ref RE: Regex = Regex::new(r"mem\[(?P<index>\d{1,})\] = (?P<value>\d{1,})").unwrap();
 }
 
-// enum MaskPoint {
-//     Same,
-//     One,
-//     Zero,
-// }
+#[derive(PartialEq, Copy, Clone)]
+enum MaskPoint {
+    X,
+    One,
+    Zero,
+}
+
+impl TryFrom<char> for MaskPoint {
+    type Error = String;
+    fn try_from(c: char) -> Result<Self, Self::Error> {
+        match c {
+            'X' => Ok(MaskPoint::X),
+            '0' => Ok(MaskPoint::Zero),
+            '1' => Ok(MaskPoint::One),
+            _ => Err(format!("Could not match {} to MaskPoint", c)),
+        }
+    }
+}
+
+impl TryFrom<MaskPoint> for char {
+    type Error = String;
+    fn try_from(c: MaskPoint) -> Result<Self, Self::Error> {
+        match c {
+            MaskPoint::X => Ok('X'),
+            MaskPoint::Zero => Ok('0'),
+            MaskPoint::One => Ok('1'),
+        }
+    }
+}
 
 enum Instruction {
-    Mask(Vec<char>),
+    Mask(Vec<MaskPoint>),
     Memory(usize, usize),
 }
 
 #[allow(dead_code, unused_variables)]
 pub fn star_one(input: impl BufRead) -> usize {
-    let mut instructions = input
-        .lines()
-        .filter_map(Result::ok)
-        .map(|line| {
-            let firstchars: String = line.chars().take(3).collect();
-            match firstchars.as_str() {
-                "mas" => Instruction::Mask(line.chars().skip(7).collect()),
-                "mem" => {
-                    let captures = RE
-                        .captures(&line)
-                        .expect(&format!("Could not match {} with regex", line));
-                    let index = captures
-                        .name("index")
-                        .unwrap()
-                        .as_str()
-                        .parse::<usize>()
-                        .unwrap();
-                    let value = captures
-                        .name("value")
-                        .unwrap()
-                        .as_str()
-                        .parse::<usize>()
-                        .unwrap();
-                    Instruction::Memory(index, value)
-                }
-                _ => panic!(),
-            }
-        })
-        .collect::<Vec<Instruction>>();
+    let mut instructions = parse_input(input).collect::<Vec<Instruction>>();
 
     let first = match instructions.remove(0) {
         Instruction::Mask(x) => x,
@@ -57,136 +54,147 @@ pub fn star_one(input: impl BufRead) -> usize {
 
     let hashmap = instructions
         .into_iter()
-        .fold((HashMap::new(), first), |(mut hm, mask), instruction| match instruction {
-            Instruction::Mask(chars) => (hm, chars),
-            Instruction::Memory(index, value) => {
-                let b: Vec<char> = (format!("{:b}", value)).chars().rev().collect();
-                let binary_str = mask
-                    .iter()
-                    .rev()
-                    .enumerate()
-                    .map(|(i, c)| match (c, b.get(i)) {
-                        ('X', Some(c)) => *c,
-                        ('X', None) => '0',
-                        ('1', _) => '1',
-                        ('0', _) => '0',
-                        _ => unreachable!(),
-                    })
-                    .rev()
-                    .collect::<String>();
-                let new_value = usize::from_str_radix(&binary_str, 2).unwrap();
-                hm.insert(index, new_value);
-                (hm, mask)
-            }
-        }).0;
-        hashmap.values().sum()
-        
+        .fold(
+            (HashMap::new(), first),
+            |(mut hm, mask), instruction| match instruction {
+                Instruction::Mask(chars) => (hm, chars),
+                Instruction::Memory(index, value) => {
+                    let b: Vec<char> = (format!("{:036b}", value)).chars().rev().collect();
+                    let binary_str = mask
+                        .iter()
+                        .rev()
+                        .enumerate()
+                        .map(|(i, c)| match (c, b.get(i)) {
+                            (MaskPoint::X, Some(c)) => *c,
+                            (MaskPoint::X, None) => '0',
+                            (MaskPoint::One, _) => '1',
+                            (MaskPoint::Zero, _) => '0',
+                        })
+                        .rev()
+                        .collect::<String>();
+                    let new_value = usize::from_str_radix(&binary_str, 2).unwrap();
+                    hm.insert(index, new_value);
+                    (hm, mask)
+                }
+            },
+        )
+        .0;
+    hashmap.values().sum()
 }
 
-fn get_addresses(mask: &Vec<char>, address: usize) -> Vec<usize> {
-    // TODO maybe use minus and subtraction of base2 values.
-    // Create lis tof masks
-    let mut values = Vec::new();
-    let b: Vec<char> = (format!("{:036b}", address)).chars().collect();
-    values.push(b);
-    
-    println!("{}", mask.len());
-    for i in 0..mask.len() {
-        match mask[i] {
-            'X' => {
-                // copy values and update old[i] to 0 and new[i] to 1
-                let new_values = values.clone();
+fn from_maskpoints(maskpoints: Vec<MaskPoint>) -> usize {
+    usize::from_str_radix(
+        &maskpoints
+            .into_iter()
+            .map(|x| match x {
+                MaskPoint::X => 'X',
+                MaskPoint::One => '1',
+                MaskPoint::Zero => '0',
+            })
+            .collect::<String>(),
+        2,
+    )
+    .unwrap()
+}
 
-                for opt in &mut values {
-                    opt[i] = '1';
-                }
+fn set_addresses(address: Vec<MaskPoint>, mut set: impl FnMut(usize)) {
+    let mut stack = Vec::new();
+    stack.push(address);
 
-                values.extend(new_values.into_iter().map(|mut opt|{
-                    opt[i] = '0';
-                    opt
-                }));
-                
-            },
-            '1' => {
-                // set all values[i] to 1,
-                for opt in &mut values {
-                    opt[i] = '1';
-                }
-            }
-            _ => {},
+    while let Some(mut address) = stack.pop() {
+        if let Some(pos) = address.iter().position(|&p| p == MaskPoint::X) {
+            let mut left = address.clone();
+            left[pos] = MaskPoint::One;
+            stack.push(left);
+            address[pos] = MaskPoint::Zero;
+            stack.push(address);
+        } else {
+            set(from_maskpoints(address));
         }
     }
+}
 
-    values.into_iter().map(|value| {
-        let s: String = value.into_iter().collect();
-        usize::from_str_radix(&s, 2).unwrap()
-    }).collect()
-    
+fn parse_input(input: impl BufRead) -> impl Iterator<Item = Instruction> {
+    input.lines().filter_map(Result::ok).map(|line| {
+        let firstchars: String = line.chars().take(3).collect();
+        match firstchars.as_str() {
+            "mas" => Instruction::Mask(
+                line.chars()
+                    .skip(7)
+                    .map(MaskPoint::try_from)
+                    .filter_map(Result::ok)
+                    .collect(),
+            ),
+            "mem" => {
+                let captures = RE
+                    .captures(&line)
+                    .expect(&format!("Could not match {} with regex", line));
+                let index = captures
+                    .name("index")
+                    .unwrap()
+                    .as_str()
+                    .parse::<usize>()
+                    .unwrap();
+                let value = captures
+                    .name("value")
+                    .unwrap()
+                    .as_str()
+                    .parse::<usize>()
+                    .unwrap();
+                Instruction::Memory(index, value)
+            }
+            _ => panic!(),
+        }
+    })
 }
 
 #[allow(dead_code, unused_variables)]
 pub fn star_two(input: impl BufRead) -> usize {
-    let mut instructions = input
-        .lines()
-        .filter_map(Result::ok)
-        .map(|line| {
-            let firstchars: String = line.chars().take(3).collect();
-            match firstchars.as_str() {
-                "mas" => Instruction::Mask(line.chars().skip(7).collect()),
-                "mem" => {
-                    let captures = RE
-                        .captures(&line)
-                        .expect(&format!("Could not match {} with regex", line));
-                    let index = captures
-                        .name("index")
-                        .unwrap()
-                        .as_str()
-                        .parse::<usize>()
-                        .unwrap();
-                    let value = captures
-                        .name("value")
-                        .unwrap()
-                        .as_str()
-                        .parse::<usize>()
-                        .unwrap();
-                    Instruction::Memory(index, value)
-                }
-                _ => panic!(),
-            }
-        })
-        .collect::<Vec<Instruction>>();
+    let mut instructions = parse_input(input).collect::<Vec<Instruction>>();
 
     let first = match instructions.remove(0) {
-        Instruction::Mask(x) => x,
+        Instruction::Mask(x) => x
+            .into_iter()
+            .enumerate()
+            .filter(|(_, x)| x != &MaskPoint::Zero)
+            .collect::<HashMap<usize, MaskPoint>>(),
         _ => unreachable!(),
     };
 
     let hashmap = instructions
         .into_iter()
-        .fold((HashMap::new(), first), |(mut hm, mask), instruction| match instruction {
-            Instruction::Mask(chars) => (hm, chars),
-            Instruction::Memory(index, value) => {
-                for new_index in get_addresses(&mask, index) {
-                    hm.insert(new_index, value);
+        .fold(
+            (HashMap::new(), first),
+            |(mut hm, mask), instruction| match instruction {
+                Instruction::Mask(chars) => (
+                    hm,
+                    chars
+                        .into_iter()
+                        .enumerate()
+                        .filter(|(_, x)| x != &MaskPoint::Zero)
+                        .collect::<HashMap<usize, MaskPoint>>(),
+                ),
+                Instruction::Memory(index, value) => {
+                    let adress_str = (format!("{:036b}", index))
+                        .chars()
+                        .enumerate()
+                        .map(|(i, x)| *mask.get(&i).unwrap_or(&MaskPoint::try_from(x).unwrap()))
+                        .collect();
+                    set_addresses(adress_str, |new_index| {
+                        hm.insert(new_index, value);
+                    });
+                    (hm, mask)
                 }
-                (hm, mask)
-            }
-        }).0;
-        hashmap.values().sum()
+            },
+        )
+        .0;
+    hashmap.values().sum()
 }
 
 #[cfg(test)]
 mod tests {
     use super::*;
     use std::io::Cursor;
-
-    #[test]
-    fn test_get_addresses() {
-        let mask = "00000000000000000000000000000000X0XX".chars().collect();
-        let mut addresses = get_addresses(&mask, 26);
-        addresses.sort();
-        assert_eq!(addresses, vec![16, 17, 18, 19, 24, 25, 26, 27]);
-    }
 
     #[test]
     fn test_star_one() {
@@ -199,10 +207,10 @@ mem[8] = 0";
 
     #[test]
     fn test_star_two() {
-        let input = b"mask = XXXXXXXXXXXXXXXXXXXXXXXXXXXXX1XXXX0X
-mem[8] = 11
-mem[7] = 101
-mem[8] = 0";
+        let input = b"mask = 000000000000000000000000000000X1001X
+mem[42] = 100
+mask = 00000000000000000000000000000000X0XX
+mem[26] = 1";
         assert_eq!(star_two(Cursor::new(input)), 208);
     }
 }
