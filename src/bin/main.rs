bca59d8afc0a0b792dbdb1b68516a8f8b1536440
Nicholas L <nicholas-l@users.noreply.github.com>
2020-12-24 13:15:19 +0000

    Update to all being 25

--- src/bin/main.rs
+++ src/bin/main.rs
@@ -8,14 +8,14 @@ use advent_of_code_2020::*;
 fn main() {
     let args: Vec<String> = env::args().skip(1).collect();
     let days = if args.len() == 1 && args[0] == "all" {
-        (1..=20).map(|x| x.to_string()).collect()
+        (1..=25).map(|x| x.to_string()).collect()
     } else {
         args
     };
     for day in days {
-        let (star_one, star_two, filename) = get_day(day.parse::<usize>().unwrap());
-
         println!("Day {}:", day);
+        
+        let (star_one, star_two, filename) = get_day(day.parse::<usize>().unwrap());
         let f = fs::File::open(&filename).unwrap();
         let input = BufReader::new(f);
         println!("Star One: {}", star_one(Box::new(input)));
